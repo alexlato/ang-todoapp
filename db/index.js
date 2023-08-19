6bd15422426375d0554a564cf92c1e6d4da610d1
alexlato <86857302+alexlato@users.noreply.github.com>
2023-08-18 20:04:07 -0700

    db connect

--- db/index.js
+++ db/index.js
@@ -6,3 +6,16 @@ const multer = require("multer");
 
 var app = Express();
 app.use(cors());
+
+var CONNECTION_STRING = "";
+
+var DATABASENAME = "todoappdb";
+
+var database;
+
+app.listen(5038, () => {
+  Mongoclient.connect(CONNECTION_STRING, (error, client) => {
+    database = client.db(DATABASENAME);
+    console.log("Mongo DB Connection Successful");
+  });
+});
